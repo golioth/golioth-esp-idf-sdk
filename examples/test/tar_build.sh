96a7213a7aa17a4cdef3ce0cf9e6455940e8fb32
Nick Miller <nick@golioth.io>
2022-06-22 11:11:12 -0700

    ci_on_hardware: separate build for OTA
    Signed-off-by: Nick Miller <nick@golioth.io>

--- examples/test/tar_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tar czf \
-    build.tar.gz \
-    build/flasher_args.json \
-    build/bootloader/bootloader.bin \
-    build/test.bin \
-    build/partition_table/partition-table.bin \
-    build/ota_data_initial.bin
