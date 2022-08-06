c159d792bd707d72865886cbd2ebbb345ab5224c
Mike Szczys <mike@golioth.io>
2022-08-06 16:19:38 -0500

    examples: common: add USB-CDC test to shell init
    * Don't try to initialize the UART console when USB-CDC console is enabled
    * Add include to fix undefined reference to vTaskList
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- examples/common/shell.c
+++ examples/common/shell.c
@@ -207,6 +207,7 @@ static void initialize_console(void) {
     setvbuf(stdin, NULL, _IONBF, 0);
     esp_vfs_dev_uart_port_set_rx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CR);
     esp_vfs_dev_uart_port_set_tx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CRLF);
+#ifndef CONFIG_ESP_CONSOLE_USB_CDC
     const uart_config_t uart_config = {
         .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
         .data_bits = UART_DATA_8_BITS,
@@ -221,6 +222,7 @@ static void initialize_console(void) {
     ESP_ERROR_CHECK(uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0));
     ESP_ERROR_CHECK(uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config));
     esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
+#endif
     esp_console_config_t console_config = {
             .max_cmdline_args = 8, .max_cmdline_length = 256, .hint_color = atoi(LOG_COLOR_CYAN)};
     ESP_ERROR_CHECK(esp_console_init(&console_config));
